6f6a6600557ad1e376b6446b3d1a900d383257b5
Mat Kelcey <matthew.kelcey@gmail.com>
2019-09-12 11:35:02 +1000

    split example numpy sum eg into own class

--- simple_ga.py
+++ simple_ga.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-
-from collections import Counter
 import numpy as np
 import random
 
@@ -20,7 +17,7 @@ class SimpleGA(object):
             raise Exception("expect proportion_new_members to be (0, 1)")
         self.num_new_members = int(self.popn_size * proportion_new_members)
 
-        self.fitness_fn = fitness
+        self.fitness_fn = fitness_fn
 
         if proportion_elite < 0 or proportion_elite > 1:
             raise Exception("expect proportion_elite to be (0, 1)")
@@ -96,35 +93,3 @@ class SimpleGA(object):
         c1 = np.concatenate([p1[:crossover_idx], p2[crossover_idx:]])
         c2 = np.concatenate([p2[:crossover_idx], p1[crossover_idx:]])
         return c1, c2
-
-# ------------------------------------------------
-
-
-def np_new_member():
-    member_size = random.randint(10, 30)
-    return np.random.normal(size=(member_size,))
-
-
-def np_crossover(p1, p2):
-    crossover_idx = random.randint(0, min(len(p1), len(p2)))
-    c1 = np.concatenate([p1[:crossover_idx], p2[crossover_idx:]])
-    c2 = np.concatenate([p2[:crossover_idx], p1[crossover_idx:]])
-    return c1, c2
-
-
-def fitness(m):
-    return np.sum(m)
-
-
-ga = SimpleGA(popn_size=10,
-              new_member_fn=np_new_member,
-              fitness_fn=fitness,
-              cross_over_fn=np_crossover,
-              proportion_new_members=0.2,
-              proportion_elite=0.1)
-
-for _ in range(100):
-    ga.calc_fitnesses()
-    elite = ga.get_elite_member()
-    print("ELITE", len(elite), np.sum(elite))
-    ga.breed_next_gen()
